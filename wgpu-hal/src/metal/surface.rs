df0cf4200822b1f5783a80138ea6ff962ddafe9f
Jinlei Li <jinleili0@outlook.com>
2022-08-10 22:20:57 +0800

    metal: remove  `setBackgroundColor`  call when using non-opaque AlphaMode.

--- wgpu-hal/src/metal/surface.rs
+++ wgpu-hal/src/metal/surface.rs
@@ -179,20 +179,7 @@ impl crate::Surface<super::Api> for super::Surface {
 
         match config.composite_alpha_mode {
             wgt::CompositeAlphaMode::Opaque => render_layer.set_opaque(true),
-            _ => {
-                render_layer.set_opaque(false);
-
-                // CALayer may have a background color by default,
-                // if the alpha_mode of the surface is set to non-opaque, unexpected colors are obtained.
-                #[cfg(target_os = "macos")]
-                let class = class!(NSColor);
-                #[cfg(target_os = "ios")]
-                let class = class!(UIColor);
-
-                let color: *mut Object = msg_send![class, clearColor];
-                let cg_color: *mut Object = msg_send![color, CGColor];
-                let () = msg_send![render_layer.as_ref(), setBackgroundColor: cg_color];
-            }
+            _ => render_layer.set_opaque(false),
         }
 
         let device_raw = device.shared.device.lock();
